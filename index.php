a02416f2d7d05a3b430258ef3d0c85d276d8fe3b
thdnz <thyagodinniz@gmail.com>
2020-04-13 19:58:39 -0300

    MVC + composer install

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<meta charset="utf-8">
+<meta name="viewport" content="widt"
+
+<head>
+    <title>Projeto Eshop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>

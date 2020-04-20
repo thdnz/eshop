d4fc3aa3ea95124f10a254b216e53ade281d6990
thdnz <thyagodinniz@gmail.com>
2020-04-20 00:44:50 -0300

    add config.class

---
+++ model/Config.class.php
@@ -0,0 +1,30 @@
+<?php
+
+Class Config(){
+
+//Informacoes Basicas do Site
+const SITE_URL = "http://localhost";
+const SITE_PASTA = "ESHO";
+const SITE_NOME = "ESHOP - PHP 7 e Mysqli";
+const SITE_EMAIL_ADM = "eshop@gmail.com";
+
+//Informações do banco de dados
+const BD_HOST = "locashost",
+      BD_USER = "root",
+      BD_SENHA = "",
+      BD_BANCO = "eshop"
+      
+
+
+//Informaçoes para PHP Mailler
+const EMAIL_HOST = "smtp.gmail.com";
+const EMAIL_USER = "eshop@gmail.com";
+const EMAIL_NOME = "Contato Eshop";
+const EMAIL_SENHA = "123456";
+const EMAIL_PORTA = "578";
+const EMAIL_SMTPAUTH = "true";
+const EMAIL_SMTPSECURE = "tls";
+
+
+}
+?>

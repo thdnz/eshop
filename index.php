3f4b987557136433a7c4e159b4b3034bc2ede8f2
thdnz <thyagodinniz@gmail.com>
2020-05-08 22:26:59 -0300

    add update

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 require "./lib/autoload.php";
 
 $smarty = new Template();
-Rotas::get_Pagina();
+
 
 //valores para o template
 $smarty->assign('NOME', 'ANANSI');
-
 $smarty->assign('GET_TEMA', Rotas::get_SiteTEMA());
+$smarty->assign('GET_HOME', Rotas::get_SiteHOME());
+$smarty->assign('PAG_CARRINHO', Rotas::pag_Carrinho());
+$smarty->assign('PAG_CONTATO', Rotas::pag_Contato());
+$smarty->assign('PAG_MINHACONTA', Rotas::pag_Minhaconta());
 
 
 
 $smarty->display('index.tpl');
-
-?>

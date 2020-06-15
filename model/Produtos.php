c09bff3dea47cdaaeef7f8034d3083b240b0aee2
thdnz <thyagodinniz@gmail.com>
2020-06-15 14:22:53 -0300

    add up

--- model/Produtos.php
+++ model/Produtos.php
@@ -2,6 +2,12 @@
 Class Produtos extends Conexao{
     function __construct()
     {
-        
+        parent::__construct();
+    }
+    function GetProdutos()   {
+        //query para buscar produtos de uma categoria especifica
+    $query = "SELECT * FROM {$this->prefix}produtos p 
+    INNER JOIN  {$this->prefix}categorias c ON p.pro_categoria
+    = c.cate_id"
     }
 }

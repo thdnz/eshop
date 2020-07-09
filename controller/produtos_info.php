ddd41a2ab4bbae13975aa5f16ceb32ca9dca0278
thdnz <thyagodinniz@gmail.com>
2020-07-08 22:40:01 -0300

    add up

--- controller/produtos_info.php
+++ controller/produtos_info.php
@@ -6,6 +6,7 @@ $produtos = new Produtos();
 $produtos->GetProdutosID(Rotas::$pag[1]);
 
 $smarty->assign('PRO', $produtos->GetItens());
+$smarty->assign('TEMA', Rotas::get_SiteTEMA());
 
 $smarty->display('produtos_info.tpl');
 
